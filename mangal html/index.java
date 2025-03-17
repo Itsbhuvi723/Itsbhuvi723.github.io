4f88bba41d354008dcef0756c9100fabc93cefcc
Itsbhuvi723 <147731040+Itsbhuvi723@users.noreply.github.com>
2025-03-17 12:23:48 +0530

    location

---
+++ mangal html/index.java
@@ -0,0 +1,26 @@
+package com.example.bustracker;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load your web app URL or local file
+        webView.loadUrl("file:///android_asset/index.html"); // If using local file
+        // OR webView.loadUrl("https://yourwebsite.com"); // If hosted online
+    }
+}
